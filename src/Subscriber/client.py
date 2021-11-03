74103426c159736e74290f696b19df4f5f4837af
Pedro Azevedo <pedrag1234@gmail.com>
2021-11-03 11:47:52 +0000

    Update client.py

--- src/Subscriber/client.py
+++ src/Subscriber/client.py
@@ -1,4 +1,35 @@
+from _typeshed import Self
 import zmq
+
+
+class client:
+    def __init__(self, pub_connection, sub_connection, sub_topic, pub_topic):
+        self.context = zmq.Context()
+        self.pub_socket = self.context.socket(zmq.PUB)
+        self.sub_socket = self.context.socket(zmq.SUB)
+        self.poller_socket = self.connect.soc
+        self.pub_connection = pub_connection
+        self.sub_connection = sub_connection
+        self.sub_topic = sub_topic
+        self.pub_topic = pub_topic
+
+    def connect(self):
+        self.pub_socket.connect(self.pub_connection)
+        self.sub_socket.connect(self.sub_connection)
+
+    def subscribe(self):
+        for topic in range(self.sub_topics):
+            self.sub_socket.setsockopt(zmq.SUBSCRIBE, topic)
+
+    def unsubscribe(self):
+        for topic in range(self.sub_topics):
+            self.sub_socket.setsockopt(zmq.UNSUBSCRIBE, topic)
+
+    def get(self):
+        self.sub_socket.recv(zmq.DONTWAIT)
+
+    def put(self, message):
+        pass
 
 
 def _main():
